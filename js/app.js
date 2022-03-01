29545beaa739e0d89b74e57ae18b2b482732904e
Mohammad Ali <itsproali@gmail.com>
2022-03-01 18:19:01 +0600

    Custom CSS Added, Load Phones before search

--- js/app.js
+++ js/app.js
@@ -1,8 +1,35 @@
 // Dom Selectors
-const mainContainer = document.getElementById('display-phones');
+const mainContainer = document.getElementById('main-container');
+const applePhones = document.getElementById('apple-phones');
+const phonesContainer = document.getElementById('display-phones');
 const searchField = document.getElementById('search-field');
 const searchButton = document.getElementById('button-addon2');
 const itemDetails = document.getElementById('item-details');
+
+// Load phones before Search
+fetch(`https://openapi.programming-hero.com/api/phones?search=${'iphone'}`)
+    .then(res => res.json())
+    .then(data => displayApples(data));
+
+// Display Phones Before Search
+const displayApples = phones => {
+    const sliced = phones.data.slice(0, 6);
+    sliced.forEach(phone => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+            <div class="card h-100 shadow p-2">
+            <img src="${phone.image}" class="card-img-top h-75 w-auto mx-auto" alt="${phone.phone_name}">
+            <div class="card-body">
+              <h5 class="card-title">${phone.phone_name}</h5>
+              <p >${phone.brand}</p>
+              <button onclick="loadItemDetails('${phone.slug}')" class="btn btn-outline-warning px-4">Details</button>
+            </div>
+          </div>
+        `;
+        applePhones.appendChild(div);
+    })
+}
 
 // Search Phone
 const searchPhone = () => {
@@ -10,14 +37,16 @@ const searchPhone = () => {
         .then(res => res.json())
         .then(data => displayPhones(data));
     searchField.value = '';
-
 }
 
 // Display Phones
 const displayPhones = phones => {
-    mainContainer.textContent = '';
+    phonesContainer.textContent = '';
+    itemDetails.textContent = '';
+    itemDetails.style.display = 'none';
+    document.getElementById('all-phones').textContent = '';
     if (phones.status === false) {
-        alert('Please try again')
+        alert('Your Search Result not Found')
     } else {
         const sliced = phones.data.slice(0, 20);
         sliced.forEach(phone => {
@@ -34,7 +63,7 @@ const displayPhones = phones => {
             </div>
           </div>
         `;
-            mainContainer.appendChild(div);
+            phonesContainer.appendChild(div);
         })
     }
 }
@@ -49,19 +78,34 @@ const loadItemDetails = itemDetails => {
 // Display Details
 const displayItemDetails = details => {
     console.log(details);
+    itemDetails.textContent = '';
+    itemDetails.style.display = 'block';
     const div = document.createElement('div');
     div.classList.add('row', 'g-0')
     div.innerHTML = `
-    <div class="col-md-5">
-        <img src="${details.image}" class="img-fluid rounded-start" alt="...">
+    <div class="col-md-4">
+        <img src="${details.image}" class="img-fluid rounded-start mx-auto" alt="${details.phone_name}">
     </div>
-    <div class="col-md-7">
+    <div class="col-md-8">
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <h5 class="card-title text-custom">${details.name}</h5>
+           <h6>Brand: ${details.brand}</h6>
+           <h6>${releaseDate(details.releaseDate)}</h6>
+           <ul>
+           <li class="mb-0">ChipSet: ${details.mainFeatures.chipSet}</li>
+           <li class="mb-0">Display: ${details.mainFeatures.displaySize}</li>
+           </ul>
         </div>
     </div>
     `;
     itemDetails.appendChild(div);
+}
+
+// Release Date Handle
+const releaseDate = releaseStatus => {
+    if (releaseStatus === '') {
+        return "No Release Date Found"
+    } else {
+        return releaseStatus;
+    }
 }
