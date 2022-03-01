385280a853e19e5b1a9fe651df7d309386472e9c
Mohammad Ali <itsproali@gmail.com>
2022-03-01 11:38:20 +0600

    API Connected

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-console.log("My name is 'app.js'. I am connected");
+// Dom Selectors
+const mainContainer = document.getElementById('display-phones');
+const searchField = document.getElementById('search-field');
+const searchButton = document.getElementById('button-addon2');
+
+// Search Phone
+const searchPhone = () => {
+    fetch(`https://openapi.programming-hero.com/api/phones?search=${searchField.value}`)
+    .then(res => res.json())
+    .then(data => displayPhones(data.data));
+
+}
+
+const displayPhones = phones => {
+    phones.forEach(phone => {
+        console.log(phone);
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <h1>${phone.phone_name}</h1>
+        `
+        mainContainer.appendChild(div);
+    });
+}
