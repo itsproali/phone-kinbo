0fb4c7fbf80b40cbee6dfd3d92fe26890de2dd98
Mohammad Ali <itsproali@gmail.com>
2022-03-01 12:58:31 +0600

    Displayed Phones

--- js/app.js
+++ js/app.js
@@ -6,18 +6,31 @@ const searchButton = document.getElementById('button-addon2');
 // Search Phone
 const searchPhone = () => {
     fetch(`https://openapi.programming-hero.com/api/phones?search=${searchField.value}`)
-    .then(res => res.json())
-    .then(data => displayPhones(data.data));
+        .then(res => res.json())
+        .then(data => displayPhones(data));
+    searchField.value = '';
 
 }
 
+// Display Phones
 const displayPhones = phones => {
-    phones.forEach(phone => {
-        console.log(phone);
-        const div = document.createElement('div');
-        div.innerHTML = `
-        <h1>${phone.phone_name}</h1>
-        `
-        mainContainer.appendChild(div);
-    });
+    if (phones.status === false) {
+        alert('Please try again')
+    } else {
+        phones.data.forEach(phone => {
+            console.log(phone);
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+            <div class="card h-100 shadow p-2">
+            <img src="${phone.image}" class="card-img-top h-75 w-auto mx-auto" alt="${phone.phone_name}">
+            <div class="card-body">
+              <h5 class="card-title">${phone.phone_name}</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+          </div>
+        `;
+            mainContainer.appendChild(div);
+        });
+    }
 }
