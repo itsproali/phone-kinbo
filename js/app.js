4b200e8c56ce535bfc714cae990547365b030afe
Mohammad Ali <itsproali@gmail.com>
2022-03-01 16:40:17 +0600

    Item Details, Navbar, Footer Added

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 const mainContainer = document.getElementById('display-phones');
 const searchField = document.getElementById('search-field');
 const searchButton = document.getElementById('button-addon2');
+const itemDetails = document.getElementById('item-details');
 
 // Search Phone
 const searchPhone = () => {
@@ -14,11 +15,13 @@ const searchPhone = () => {
 
 // Display Phones
 const displayPhones = phones => {
+    mainContainer.textContent = '';
     if (phones.status === false) {
         alert('Please try again')
     } else {
-        phones.data.forEach(phone => {
-            console.log(phone);
+        const sliced = phones.data.slice(0, 20);
+        sliced.forEach(phone => {
+            // console.log(phone);
             const div = document.createElement('div');
             div.classList.add('col');
             div.innerHTML = `
@@ -26,11 +29,39 @@ const displayPhones = phones => {
             <img src="${phone.image}" class="card-img-top h-75 w-auto mx-auto" alt="${phone.phone_name}">
             <div class="card-body">
               <h5 class="card-title">${phone.phone_name}</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p >${phone.brand}</p>
+              <button onclick="loadItemDetails('${phone.slug}')" class="btn btn-outline-warning px-4">Details</button>
             </div>
           </div>
         `;
             mainContainer.appendChild(div);
-        });
+        })
     }
+}
+
+// Load Details
+const loadItemDetails = itemDetails => {
+    fetch(`https://openapi.programming-hero.com/api/phone/${itemDetails}`)
+        .then(res => res.json())
+        .then(data => displayItemDetails(data.data))
+}
+
+// Display Details
+const displayItemDetails = details => {
+    console.log(details);
+    const div = document.createElement('div');
+    div.classList.add('row', 'g-0')
+    div.innerHTML = `
+    <div class="col-md-5">
+        <img src="${details.image}" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-7">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+    </div>
+    `;
+    itemDetails.appendChild(div);
 }
